80f5de4b6538b61d51669fd4fe78dcda8fe29653
Shak Stick <sanshakurt@gmail.com>
2022-07-24 00:46:37 +0430

    complete the project

---
+++ index.js
@@ -0,0 +1,16 @@
+const quoteSection = document.querySelector("blockquote");
+const changer = document.querySelector(".dice");
+const idSpan = document.querySelector("span");
+const url = "https://api.adviceslip.com/advice";
+
+quoteGenerator();
+
+changer.addEventListener("click", quoteGenerator);
+
+function quoteGenerator() {
+  fetch(url)
+    .then((response) => response.json())
+    .then(
+      (responseJson) => (quoteSection.textContent = responseJson.slip.advice)
+    );
+}
